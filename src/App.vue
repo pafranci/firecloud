ba6619d4da348df85fcb599386d1892619f231d0
pafranci <pjafrancisco1@gmail.com>
2020-08-15 18:25:18 +0930

    Dashboard

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,106 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>FirePath NSW Bushfire Live Status</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+      <v-row dense class="justify-center">
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+            <v-btn
+              :href="source"
+              icon
+              large
+              target="_blank"
+              v-on="on"
+            >
+              <v-icon>mdi-map</v-icon>
+            </v-btn>
+          </template>
+          <span>Map</span>
+        </v-tooltip>
+      </v-row>
+
+      <!--fire-danger-index :highlights="highlights"></fire-danger-index-->
+
+        <v-row dense>
+          <v-col
+            v-for="card in cards"
+              :key="card.title"
+              :cols="card.flex"
+            class="text-center"
+          >
+            <v-card>
+              <!--v-img
+                :src="card.src"
+                class="white--text align-end"
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                height="200px"
+              -->
+                <v-card-text v-text="card.title"></v-card-text>
+                <v-card-title v-text="card.value" class="justify-center"></v-card-title>
+
+              <!--/v-img-->
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+    <v-footer
+      color="indigo"
+      app
+    >
+      <span class="white--text">FireTec &copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  //import FireDangerIndex from "./FireDangerIndex.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    props: {
+      source: String,
+    },
+    components:{
+      //"fire-danger-index": FireDangerIndex,
+    },
+    data: () => ({
+      cards: [
+        { title: 'Status', value: 'Fire Ongoing', flex: 3  },
+        { title: 'Fire Danger Index', value: '2', flex: 6 },
+        { title: 'Last Rainfall', value: '6 days ago', flex: 3 },
+        { title: 'Temperature (C)', value: '30', flex: 3 },
+        { title: 'Wind Speed (km/hr)', value: '11 km/h', flex: 3  },
+        { title: 'Wind Direction', value: 'WSW', flex: 3  },
+        { title: 'Humidity (%)', value: '81', flex: 3 },
+      ],
+    }),
+    mounted(){
+      console.log("mounted");
+      console.log(this.cards[0]['value']);
+      var self = this;
+      var windDirection = ['N','NNE','NE','ENE','E','ESE','SE','SSE','S','SSW','SW','WSW','W','WNW','NW','NNW']
+      setInterval(function(){ 
+        console.log("setInterval");
+        self.cards[1].value = Math.floor((Math.random() * 10) + 1);
+        self.cards[3].value = Math.floor((Math.random() * 40) + 30);
+        self.cards[4].value = Math.floor((Math.random() * 10) + 1);
+        self.cards[5].value = windDirection[Math.floor((Math.random() * 16))];
+        self.cards[6].value = Math.floor((Math.random() * 100) + 1);
+        }, 
+        3000);
+    },
+    
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import Widgets from 'fusioncharts/fusioncharts.widgets';
+import PowerCharts from 'fusioncharts/fusioncharts.powercharts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+import VueFusionCharts from 'vue-fusioncharts';
+
+// Resolve the dependencies
+Charts(FusionCharts);
+PowerCharts(FusionCharts);
+Widgets(FusionCharts);
+FusionTheme(FusionCharts);
+
+// Globally register the components for project-wide use
+Vue.use(VueFusionCharts, FusionCharts);
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
